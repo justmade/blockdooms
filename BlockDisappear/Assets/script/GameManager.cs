f43059370ecc1cde5be4177a3ab0ab4082748691
shitianyun <77976248@qq.com>
2018-03-04 18:27:34 +0800

    init

---
+++ BlockDisappear/Assets/script/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject m_BlockPrefabs;
+
+	public List<GameObject> allBlocks;
+
+	// Use this for initialization
+	void Start () {
+		initAllBlock ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void initAllBlock(){
+		allBlocks = new List<GameObject> ();
+		Vector3 v = new Vector3 (0,1,0);
+		Quaternion turnRotation= Quaternion.Euler (0f, 0f, 0f);
+		for (int i = 0; i < 9; i++)
+		{
+			v.x = Mathf.Ceil (i / 3) * 1.1f;
+			v.z = i%3 * 1.1f ;
+			// ... create them, set their player number and references needed for control.
+			GameObject block = 
+				Instantiate(m_BlockPrefabs, v, turnRotation) as GameObject;
+			allBlocks.Add (block);
+			block.transform.localScale = new Vector3 (0.3f, 0.3f, 0.3f);
+
+		}	
+	}
+}
---
+++ BlockDisappear/Assets/script/PlayerMove.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	private Vector3 targetPosition = new Vector3(0f,1f,0f);
+
+	private float mapWidth = 4.5f;
+
+	private bool isMoving = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void FixedUpdate()
+	{
+		getPlayerDirection ();
+	}
+
+	private void getPlayerDirection()
+	{
+		if (!isMoving) {
+			if (Input.GetKey (KeyCode.W)) {
+				targetPosition = new Vector3 (-mapWidth, 1f, transform.position.z);
+			} else if (Input.GetKey (KeyCode.S)) {
+				targetPosition = new Vector3 (mapWidth, 1f, transform.position.z);
+			} 
+
+			if (Input.GetKey (KeyCode.A)) {
+				targetPosition = new Vector3 (transform.position.x, 1f, -mapWidth);
+			} else if (Input.GetKey (KeyCode.D)) {
+				targetPosition = new Vector3 (transform.position.x, 1f, mapWidth);
+			}
+		}
+
+
+		if (targetPosition != transform.position && targetPosition != null && isMoving == false) {
+			isMoving = true;
+		}
+
+		if (targetPosition == transform.position) {
+			Debug.Log ("stop");
+			isMoving = false;
+		}
+
+		if (isMoving) {
+			float step = 20.0f * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, targetPosition, step);
+		}
+
+	}
+
+
+
+
+}
