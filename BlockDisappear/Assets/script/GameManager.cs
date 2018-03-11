bb6263529f749572d273bb6494fea24743bde116
shitianyun <77976248@qq.com>
2018-03-11 10:18:55 +0800

    Block的检索功能

--- BlockDisappear/Assets/script/GameManager.cs
+++ BlockDisappear/Assets/script/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour {
 
 	public List<GameObject> allBlocks;
 
+	public List<int> allDisappearIndex;
+
 	// Use this for initialization
 	void Start () {
 		initAllBlock ();
@@ -46,6 +48,7 @@ public class GameManager : MonoBehaviour {
 
 			if(Physics.Raycast (ray,out hit))    //如果真的发生了碰撞，ray这条射线在hit点与别的物体碰撞了
 			{
+				allDisappearIndex.Clear ();
 				if (hit.collider.gameObject.tag == "Block") {
 					Transform t = hit.collider.gameObject.transform;
 					float px = t.localPosition.x - 0.5f;
@@ -53,16 +56,91 @@ public class GameManager : MonoBehaviour {
 					int ix = Mathf.CeilToInt (px / 1);
 					int iy = Mathf.CeilToInt(pz % 5);
 					int index = ix * 5 + iy;
-					Debug.Log (index);
 
 					BlockBase bb = hit.collider.GetComponent<BlockBase> ();
 
-					Debug.Log (bb.getColorIndex ());
+					addDisappearIndex (index);
+					findNeighbour (index, bb.getColorIndex (),0);
 				}
-				
+
+				foreach (int element in allDisappearIndex) 
+				{
+					Debug.Log ("element:");
+					Debug.Log (element);
+
+				}
+
 
 			}
 		}
 
+
 	}
+
+
+
+	void findNeighbour(int index , int color , int currentDir)
+	{
+		int rightIndex = index + 5;
+
+		int leftIndex = index - 5;
+
+		int downIndex = -1;
+		if (index % 5 != 0) {
+			downIndex = index - 1;
+		}
+
+		int upIndex = -1;
+		if ((index + 1) % 5 != 0) {
+			upIndex = index + 1;
+		}
+
+		if (rightIndex < allBlocks.Count && currentDir != 8) {
+			if (getBlockColor (rightIndex) == color) {
+				if (addDisappearIndex (rightIndex)) {
+					findNeighbour (rightIndex, color , 4);
+				}
+			}
+		}
+
+		if (leftIndex >= 0 && currentDir != 4) {
+			if (getBlockColor (leftIndex) == color) {
+				if(addDisappearIndex(leftIndex)){
+					findNeighbour (leftIndex, color,8);
+				}
+			}
+		}
+
+		if (downIndex >= 0 && getBlockColor (downIndex) == color && currentDir != 1) {
+			if (addDisappearIndex (downIndex)) {
+				findNeighbour (downIndex, color,2);
+			}
+		}
+
+		if (upIndex >= 0 && getBlockColor (upIndex) == color && currentDir != 2) {
+			if (addDisappearIndex (upIndex)) {
+				findNeighbour (upIndex, color,1);
+			}
+		}
+
+		return;
+	}
+
+	bool addDisappearIndex(int index){
+		if (allDisappearIndex.IndexOf (index) == -1) {
+			allDisappearIndex.Add (index);
+			return true;
+		}
+		return false;
+	}
+
+	int getBlockColor(int index){
+		BlockBase block = allBlocks[index].GetComponent<BlockBase> ();
+		return block.getColorIndex ();
+	}
+
+
+
+
+
 }
