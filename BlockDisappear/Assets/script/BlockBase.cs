63492cac3d4830cc16f0d16e14e10f53d64cbb9f
shitianyun <77976248@qq.com>
2018-03-22 22:05:00 +0800

    新增下落和左移动画

--- BlockDisappear/Assets/script/BlockBase.cs
+++ BlockDisappear/Assets/script/BlockBase.cs
@@ -8,6 +8,22 @@ public class BlockBase : MonoBehaviour {
 
 	private int colorIndex;
 
+	private bool isDrop;
+
+	private bool isMove;
+
+	private int dropDistance;
+
+	private int moveDistance;
+
+	private Vector3 lastP;
+
+	private float timeDelta = 0f;
+
+	private float totalMove = 10f;
+
+	public AnimationCurve AniX;
+
 	public Vector3 targetPosition;
 	// Use this for initialization
 	void Awake(){
@@ -16,7 +32,7 @@ public class BlockBase : MonoBehaviour {
 	}
 
 	void Start () {
-
+		
 		this.GetComponent<Renderer> ().material = _materials [colorIndex];
 
 	}
@@ -27,7 +43,24 @@ public class BlockBase : MonoBehaviour {
 //			Vector3 v = new Vector3 (0, 0, 0.2f);
 //			transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref v, 0.1f);
 //		}
-
+		if (isDrop) {
+			if (timeDelta <= totalMove) {
+				this.transform.position = lastP - new Vector3 (0, 0, AniX.Evaluate (timeDelta / totalMove) * dropDistance);
+				timeDelta += 1f;
+			} else {
+				isDrop = false;
+				timeDelta = 0f;
+				lastP = this.transform.position;
+			}
+		} else if(isMove){
+			if (timeDelta <= totalMove) {
+				this.transform.position = lastP - new Vector3 (AniX.Evaluate (timeDelta / totalMove) * moveDistance, 0,0);
+				timeDelta += 1f;
+			} else {
+				isMove = false;
+				timeDelta = 0f;
+			}
+		}
 	}
 
 	public int getColorIndex(){
@@ -35,10 +68,18 @@ public class BlockBase : MonoBehaviour {
 	}
 
 	public void dropBlock(int distance){
-		this.transform.position = this.transform.position - new Vector3 (0, 0, distance);
+		isDrop = true;
+		dropDistance = distance;
+		//this.transform.position = this.transform.position - new Vector3 (0, 0, distance);
+		lastP = this.transform.position;
 	} 
 
 	public void leftMoveBlock(int distance){
-		this.transform.position = this.transform.position - new Vector3 (distance, 0, 0);
+		isMove = true;
+		moveDistance = distance;
+		if (!isDrop) {
+			lastP = this.transform.position;
+		}
+//		this.transform.position = this.transform.position - new Vector3 (distance, 0, 0	);
 	}
 }
