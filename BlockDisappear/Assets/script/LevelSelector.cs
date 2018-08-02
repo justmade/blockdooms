75532226fe0a63bd1091c8151a232189af311c4e
shitianyun <77976248@qq.com>
2018-08-02 19:28:52 +0800

    准备关卡界面

---
+++ BlockDisappear/Assets/script/LevelSelector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using LitJson;
+using System.IO;
+using LFormat;
+
+public class LevelSelector : MonoBehaviour {
+
+	private List<string> files;
+
+	// Use this for initialization
+	void Start () {
+		GetAllLevel ();
+	}
+
+	void GetAllLevel(){
+		files = new List<string> ();
+		foreach (var path in Directory.GetFiles(Application.dataPath+"/levels/")) {
+			if (System.IO.Path.GetExtension (path) == ".txt") {
+				string name =  (System.IO.Path.GetFileName(path));
+				Debug.LogFormat ("file {0}", name);
+				files.Add (name);
+			}
+		}
+	}
+
+	void OnGUI () 
+	{
+
+
+		//EditorGUILayout.VerticalScope;
+		string[] floorNames = new string[files.Count];
+		for (int i = 0; i < files.Count; i++) {
+			floorNames [i] = (files[i]).ToString ();;
+		}
+
+		GUILayout.SelectionGrid (1, floorNames, 3);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
