d68bb9371a5d8863bb3bd48052a09da4aaea078e
shitianyun <77976248@qq.com>
2018-08-04 17:07:58 +0800

    Add levelselect gui

--- BlockDisappear/Assets/script/LevelSelector.cs
+++ BlockDisappear/Assets/script/LevelSelector.cs
@@ -11,8 +11,35 @@ public class LevelSelector : MonoBehaviour {
 
 	private List<string> files;
 
+	private float stageW;
+	private float stageH;
+
+	private int selectIndex;
+
+	private int lastSelectIndex;
+
 	// Use this for initialization
 	void Start () {
+		selectIndex = 0;
+		lastSelectIndex = 0;
+		float leftBorder;
+		float rightBorder;
+		float topBorder;
+		float downBorder;
+		//the up right corner
+		Vector3 cornerPos=Camera.main.ViewportToWorldPoint(new Vector3(1f,1f,Mathf.Abs(Camera.main.transform.position.z)));
+
+		leftBorder=Camera.main.transform.position.x-(cornerPos.x-Camera.main.transform.position.x);
+		rightBorder=cornerPos.x;
+		topBorder=cornerPos.y;
+		downBorder=Camera.main.transform.position.y-(cornerPos.y-Camera.main.transform.position.y);
+
+		stageW=rightBorder-leftBorder;
+		stageH=topBorder-downBorder;
+
+		Debug.LogFormat ("stageW {0},{1}", stageW, stageH);
+
+
 		GetAllLevel ();
 	}
 
@@ -29,20 +56,20 @@ public class LevelSelector : MonoBehaviour {
 
 	void OnGUI () 
 	{
-
-
-		//EditorGUILayout.VerticalScope;
+		GUI.BeginGroup(new Rect(Screen.width/2 - 300/2,150,300,300));
 		string[] floorNames = new string[files.Count];
 		for (int i = 0; i < files.Count; i++) {
 			floorNames [i] = (files[i]).ToString ();;
 		}
-
-		GUILayout.SelectionGrid (1, floorNames, 3);
+		selectIndex = GUILayout.SelectionGrid (selectIndex, floorNames, 3,GUILayout.Width(300));
+		GUI.EndGroup();
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		
+		if (lastSelectIndex != selectIndex) {
+			lastSelectIndex = selectIndex;
+		}
 	}
 }
