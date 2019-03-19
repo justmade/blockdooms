c79e3191c3be2ec76d5d780b5cc067ccf3d30101
shitianyun <77976248@qq.com>
2019-03-19 22:08:47 +0800

    Added block tween

---
+++ BlockDisappear/Assets/FloatingSquare.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class FloatingSquare : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 startPos = this.transform.localPosition;
+		Vector3 toPos = new Vector3(startPos.x, startPos.y+0.2f,startPos.z);
+
+		float dur = 3f + UnityEngine.Random.Range(1f,3f);
+		Tween myTween = transform.DOLocalMove(toPos, dur);
+		myTween.SetDelay(UnityEngine.Random.Range(0f,2f));
+		myTween.SetEase(Ease.OutSine);
+		myTween.SetLoops(-1,LoopType.Yoyo);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
