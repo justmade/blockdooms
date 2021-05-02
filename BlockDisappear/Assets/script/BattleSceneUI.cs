0f62e03f940f0cb4255a7791b0bbe8abfb0729d0
Tianyun Shi <77976248@qq.com>
2021-05-02 20:55:42 +0800

    Add Tutor UI into BattleScene

---
+++ BlockDisappear/Assets/script/BattleSceneUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleSceneUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var dialogueUI = Instantiate(Resources.Load("UI/DialogueUIView")) as GameObject;
+
+        GameObject canvas = GameObject.Find("MainSceneCanvans") as GameObject;
+		dialogueUI.transform.SetParent(canvas.transform);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
